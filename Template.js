ce7500d6b484ce9f83388313f7418820a3035edc
Jared Saul <JaronOrchard@gmail.com>
2020-12-08 22:59:29 -0800

    Update template

--- Template.js
+++ Template.js
@@ -3,6 +3,7 @@ function dayXX(input) {
 }
 
 function deepCopy(input) { return JSON.parse(JSON.stringify(input)); }
+function stringContains(inputStr, subStr) { return inputStr.indexOf(subStr) !== -1; }
 
 dayXX(
 
