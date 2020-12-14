0f115deb20c7963c97a4003da83f53e68b4dd278
Jared Saul <JaronOrchard@gmail.com>
2020-12-13 21:41:35 -0800

    Update template

--- Template.js
+++ Template.js
@@ -2,8 +2,13 @@ function dayXX(input) {
   
 }
 
+/** ========== Helper functions ========== */
+function bin2dec(bin) { return parseInt(bin, 2); }
+function dec2bin(dec) { return (dec >>> 0).toString(2); }
 function deepCopy(input) { return JSON.parse(JSON.stringify(input)); }
+function replaceCharAt(inputStr, index, character) { return inputStr.substr(0, index) + character + inputStr.substr(index + 1); }
 function stringContains(inputStr, subStr) { return inputStr.indexOf(subStr) !== -1; }
+/** ====================================== */
 
 dayXX(
 
