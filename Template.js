9533ab7c65500daeb8a2155768b4f28d7f2085ac
Jared Saul <JaronOrchard@gmail.com>
2020-12-21 23:16:26 -0800

    Update template

--- Template.js
+++ Template.js
@@ -3,14 +3,18 @@ function dayXX(input) {
 }
 
 /** ========== Helper functions ========== */
+// Arrays
 function arrayContains(inputArr, val) { return inputArr.indexOf(val) !== -1; }
 function arrayIntersection(array1, array2) { return array1.filter(value => array2.includes(value)); }
-function bin2dec(bin) { return parseInt(bin, 2); }
-function dec2bin(dec) { return (dec >>> 0).toString(2); }
-function deepCopy(input) { return JSON.parse(JSON.stringify(input)); }
 function removeItemFromArray(inputArr, val) { if (arrayContains(inputArr, val)) { inputArr.splice(inputArr.indexOf(val), 1); } }
+// Strings
 function replaceCharAt(inputStr, index, character) { return inputStr.substr(0, index) + character + inputStr.substr(index + 1); }
 function stringContains(inputStr, subStr) { return inputStr.indexOf(subStr) !== -1; }
+// Binary
+function bin2dec(bin) { return parseInt(bin, 2); }
+function dec2bin(dec) { return (dec >>> 0).toString(2); }
+// Other
+function deepCopy(input) { return JSON.parse(JSON.stringify(input)); }
 /** ====================================== */
 
 dayXX(
